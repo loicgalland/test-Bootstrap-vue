02b4cfbaaf4be8eb5637507ac0f458ad209afe28
loicgalland <loicgalland1@gmail.com>
2023-03-09 10:36:24 +0100

    Ajout des fichiers

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <NavBar />
+  </div>
+  <div class="container">
+    <div class="row">
+        <CardsHotels 
+          v-for="hotel in hotels"
+          :title="hotel.title"
+          :description="hotel.description"
+          :id="hotel.id"
+          :image="hotel.image"
+          :key="hotel.title"/>
+    </div>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardsHotels from './components/cards.vue'
+import NavBar from './components/navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardsHotels,
+    NavBar
+  },
+  data() {
+  return {
+          adress: "15 rue du générale de Gaulle 71200 Charolles",
+          hotels: [
+          {id: 1, title: "Hotel F1", description: "Un peu miteux mais mieux que rien !", image: "https://picsum.photos/200/100"},
+          {id: 2, title: "Ibis", description: "Un lit confortable.", image: "https://picsum.photos/200/100"},
+          {id: 3, title: "Palacio", description: "Un vrai palace, tout est fait pour vous rendre heureux !", image: "https://picsum.photos/200/100"},
+          {id: 4, title: "La Cabane", description: "Une expérience insolite, dormir dans un arbre.", image: "https://picsum.photos/200/100"},
+          {id: 5, title: "Grotte", description: "N'ayez pas peur de dormir dans un trou !", image: "https://picsum.photos/200/100"},
+          {id: 6, title: "Jungle", description: "Des plantes, des plantes et des plantes.", image: "https://picsum.photos/200/100"}
+          ]
+      }
   }
 }
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/cards.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card p-0" style="width: 18rem;">
+    <img :src="image" :alt="image" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">{{ description }}</p>
+      <button class="btn btn-primary"> En savoir plus</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardsHotels',
+  props:["title", "description", "id", "image"]
+}
+</script>
+
+<style scoped lang="scss">
+.card{
+  margin:10px 10px 0px 10px;
+}
+</style>
